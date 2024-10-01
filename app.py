168dcf4839c5a3e0d8d3b4214fd39fa39a1fa821
AllAboutDevOps <allaboutdevops@AllAboutDevOps.local>
2024-10-02 01:39:13 +0530

    Adding sample frontend service

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get-welcome')
+def get_welcome():
+    response = requests.get('http://127.0.0.1:5001/api/welcome')
+    return response.json()
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)  # Run on port 5000
